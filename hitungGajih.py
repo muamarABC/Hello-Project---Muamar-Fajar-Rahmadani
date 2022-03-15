59725a263182d7545684849d781c1be046465049
Fajar <muamarfajar9@gmail.com>
2022-03-15 09:07:26 +0800

    Gimana hitungGaji

---
+++ hitungGajih.py
@@ -0,0 +1,13 @@
+Nip = int(input("Masukkan nomor NIP anda ! :"))
+Golongan = input("Anda Golongan Apa ? (A/B/C/D) :")
+
+if(Golongan == "A") :
+    print("Gajih Pokok Anda sebesar 2000000")
+elif(Golongan == "B") :
+    print("Gajih Pokok Anda sebesar 3000000")
+elif(Golongan == "C") :
+    print("Gajih Pokok Anda sebesar 4000000")
+elif(Golongan == "D") :
+    print("Gajih Pokok Anda sebesar 5000000")
+else :
+    print("Anda bukan Pegawai")
